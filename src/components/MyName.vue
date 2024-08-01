a2fd62dd1dc7628094361c8671a133be5a0317ef
DonghaonQin <dillion0504@gmail.com>
2024-08-01 23:00:55 +1000

    Initial commit

---
+++ src/components/MyName.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="my-name">
+    <h1>Hello, my name is {{ name }}!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName',
+  data() {
+    return {
+      name: 'Donghao Qin'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.my-name {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
