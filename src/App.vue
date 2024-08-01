a34cb63d72cd2c994d1b5a4bd43b2606ec4e85bb
DonghaonQin <dillion0504@gmail.com>
2024-08-01 23:11:52 +1000

    change code

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,19 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import MyName from './components/MyName.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-  <!-- aaaaa -->
-  <main>
-    <TheWelcome />
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
     <MyName />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import MyName from './components/MyName.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+export default {
+  name: 'App',
+  components: {
+    MyName
   }
 }
-</style>
+</script>
+
+<style></style>
